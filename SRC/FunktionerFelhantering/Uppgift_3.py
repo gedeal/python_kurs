7ab8e2f53fc3b12f58d85ddbfbf734f41f87cb75
Gerson <gerson_a@hotmail.com>
2026-04-03 20:42:34 +0200

    felhantering _v3

--- SRC/FunktionerFelhantering/Uppgift_3.py
+++ SRC/FunktionerFelhantering/Uppgift_3.py
@@ -1,15 +1,18 @@
 import os
 import subprocess
 
-def clear_console():
+
+def clear_console() -> None:
     cmd = "cls" if os.name == "nt" else "clear"
-    subprocess.call(cmd, shell=True)
+    subprocess.run(cmd, shell=True)
+
+
 
 clear_console()
 
+
 ## ---------------------------------------------------
 # Menyprogram med funktioner
-
 numbers = []
 
 
@@ -17,27 +20,43 @@ def summa_lista(lista):
     ans = sum(lista)
     print('Sum:', ans)
 
+
 def medelvarde(lista):
     adding=0
     for item in lista:
         adding = adding + item
-    print(f'medelvärde: {adding/2}')
+    print(f'Midle value: {adding/2}')
 
 
 def max_varde(lista):
     lista.sort(reverse=True)
-    print(f'Sorted lista : {lista}')
-    print(f'max_varde: {lista[0]}')
+    #print(f'Sorted lista : {lista}')
+    print(f'Max value: {lista[0]}')
+
 
 
 print('Enter numbers (to finish enter: * )')
+
+
+def Control_entry(len):
+    if len == 0:
+        # Vi skapar ett ValueError med ett eget meddelande
+        raise ValueError("Array can not be zero !")
+    return len
+
+
 while True:
     nr = input('   Enter a number : ')
+
     if nr == '*':
-        #print('EXIT')
-        break
-    else:
         try:
+            Control_entry(len(numbers))
+            break
+        except ValueError as e:
+            print(f"Validation error: {e}")
+
+    else:
+        try:   # Fel inmatning (text istället för heltal)
             int_nr = int(nr)
             numbers.append(int_nr)
 
@@ -45,25 +64,34 @@ while True:
             print("  *****>  ERROR : ", repr(e))
             #exit(0)
 
-    #print('Test Array ', numbers)
 
 lennumbers = len(numbers)
-print('\n ---------------------------')
+print('\n---------------------------')
 #print('Array length : ',lennumbers)
+print('Array : ', numbers)
+#print('---------------------------')
 
-print('Entered Array ', numbers)
-
-"""
-TBD 
-Anropa rätt funktion beroende på val
-Fel input (text istället för tal)
-Tom lista
-Fel menyval
-"""
+## ---------------------------------------------------------------------
+clear_console()   ## Does not work :-(  !!
 
 
-summa_lista(numbers)
-
-medelvarde(numbers)
-
-max_varde(numbers)
+while True:
+    print('---------------------------')
+    print('   What you want to see ?\n---------------------------')
+    print('      [ x ]   Sum of the list')
+    print('      [ y ]   Midle value of the list')
+    print('      [ z ]   Max value of the list')
+    print('      [ * ]   Finish')
+    expr = input(' => ')
+    match expr:
+        case 'x':
+            summa_lista(numbers)
+        case 'y':
+            medelvarde(numbers)
+        case 'z':
+            max_varde(numbers)
+        case '*':
+            print ('  ByBy :-)  ')
+            break
+        case _:
+            print ('Not a valid option')
